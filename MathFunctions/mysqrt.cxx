76287bf62689e6d0becc767790b861729c4e1054
Yannick Sereckissy-Namboy <ysereckissy@markem-imaje.com>
2021-11-23 16:23:33 +0100

    step 6 adding a custom command and generated file

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,19 +1,19 @@
 #include <iostream>
 
+#include "Table.h"
+
 // a hack square root calculation using simple operations
 double mysqrt(double x)
 {
   if (x <= 0) {
     return 0;
   }
-
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = exp(log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result 
-            << " using log and exp" << std::endl;
-#else
   double result = x;
-
+  if(x >= 1 && x < 10) 
+  {
+    std::cout << "use the table to help find an initial value " << std::endl;
+    result = sqrtTable[static_cast<int>(x)];
+  }
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
     if (result <= 0) {
@@ -23,6 +23,5 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-#endif
   return result;
 }
--- tutorial.cxx
+++ tutorial.cxx
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "TutorialConfig.h"
+#include "MathFunctions.h"
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +18,11 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // calculate square root
+  #ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+  #else
   const double outputValue = sqrt(inputValue);
+  #endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
